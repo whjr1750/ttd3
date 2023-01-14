756fba66ac1a1e65baa5977f264a5364a6a40b00
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2023-01-14 11:37:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 500,
+    height: 500,
+    image_format: 'png',
+    png_quality: 100,
+    force_flash: true
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
